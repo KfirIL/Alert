3cfe68d6fedb0a53dab7342f88401d9aa1412128
Kfir <royalplusto@gmail.com>
2023-04-08 21:02:00 +0300

    init

---
+++ Commands/set-alerts-channel.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const fs = require('node:fs');
+const path = require('node:path');
+
+const { SlashCommandBuilder, Integration, IntegrationApplication, InteractionResponse , PermissionFlagsBits } = require('discord.js');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('הגדרת-חדר-התראות')
+    .setDescription('חדר שבו יוצגו ההתראות')
+    .addChannelOption(option =>
+		option.setName('הערוץ-בו-תרצה-לקבל-התראות')
+			.setDescription('הכנס את הערוץ שבו אתה רוצה לקבל את ההתראות'))
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    async execute(interaction) {
+        const serverID = interaction.guild.id;
+        const channelID = interaction.options.getChannel('הערוץ-בו-תרצה-לקבל-התראות').id;
+
+        fs.readFile('channelServer.json', 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            // Parse the JSON data
+            let jsonData;
+            try {
+              jsonData = JSON.parse(data);
+            } catch (e) {
+              // Handle the case where the data is not valid JSON
+              console.error('Error parsing JSON:', e);
+              return;
+            }
+          
+            // Check if the server already exists
+            if (jsonData[serverID]) {
+              // Update the channel ID for the existing server
+              jsonData[serverID].channel = channelID;
+              console.log(`Channel ID updated for server ${serverID}`);
+            } else {
+              // Generate a new object with the new data
+              const newObj = {
+                [serverID]: {
+                  channel: channelID,
+                },
+              };
+          
+              // Add the new object to the existing JSON object
+              Object.assign(jsonData, newObj);
+            }
+          
+            // Stringify the modified data
+            const newData = JSON.stringify(jsonData);
+          
+            // Write the stringified data back to the file
+            fs.writeFile('channelServer.json', newData, 'utf8', err => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+            });
+          });
+
+        await interaction.reply({ content: 'החדר הוגדר בהצלחה', ephemeral: true });
+	},
+};
---
+++ index.js
@@ -0,0 +1,103 @@
+require('dotenv').config();
+const fs = require('node:fs');
+const path = require('node:path');
+
+const { Client, GatewayIntentBits, EmbedBuilder, Events} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+});
+
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  
+  setInterval(async () => {
+    await fetch(
+      "https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=he&mode=1"
+    )
+    .then((result) => result.json())
+    .then((data) => {
+  const givenDate = new Date('April 8, 2023 12:00:00');
+  const currentDate = new Date();
+  if (Math.abs(currentDate - givenDate) < 2000)
+  fs.readFile('channelServer.json', 'utf8', (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const json = JSON.parse(res);
+    
+    for (let server in json) {
+     
+        if(client.channels.cache.has(json[server].channel)) {
+          let channel = client.channels.cache.get(json[server].channel);
+
+              data.forEach((alert) => {
+                const embed = new EmbedBuilder()
+                  .setColor("#e8793f")
+                  .setTitle(`התראת פיקוד העורף ב–${alert.data}`)
+                  .setURL("https://www.oref.org.il//12481-he/Pakar.aspx")
+                  .setAuthor({
+                    name: "פיקוד העורף",
+                    iconURL:
+                      "https://cdn.discordapp.com/attachments/776039568163995649/1094287528451915906/Pakar.png",
+                    url: "https://www.oref.org.il//12481-he/Pakar.aspx",
+                  })
+                  .setDescription(alert.category_desc)
+                  .addFields(
+                    { name: `התראה של פיקוד העורף בשעה–${alert.time}`, value: alert.date },
+                  );
+                channel.send({ embeds: [embed] });
+              });
+        }
+    };
+  });
+  });
+}, 2000);
+});
+
+client.commands = new Map();
+const commandsPath = path.join(__dirname, 'Commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'לא יכלנו לבצע את הפקודה! דווח על זה בבקשה בשרת הדיסקורד שלנו', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'לא יכלנו לבצע את הפקודה! דווח על זה בבקשה בשרת הדיסקורד שלנו', ephemeral: true });
+		}
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN);
