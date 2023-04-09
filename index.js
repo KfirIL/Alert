8ab5762be423097e0125dc0c20dee2c04bfba423
Kfir <royalplusto@gmail.com>
2023-04-10 01:44:36 +0300

    fixed stupid shit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const {
   Events,
   ActivityType,
 } = require("discord.js");
-const { error } = require("node:console");
 
 const client = new Client({
   intents: [
@@ -22,102 +21,100 @@ const client = new Client({
   ],
 });
 
-client.on(
-  "ready",
-  async () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity({
-      name: "תשעה מיליון אנשים",
-      type: ActivityType.Watching,
-    });
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity({
+    name: "תשעה מיליון אנשים",
+    type: ActivityType.Watching,
+  });
 
-    setInterval(async () => {
-      await fetch("https://www.oref.org.il/WarningMessages/alert/alerts.json", {
-        method: "GET",
-        headers: {
-          Accept: "*/*",
-          "Accept-Encoding": "gzip, deflate, br",
-          "Accept-Language": "en-US,en;q=0.5",
-          Connection: "keep-alive",
-          "Content-Type": "application/json;charset=utf-8",
-          "If-Modified-Since": new Date().toUTCString(),
-          "If-None-Match": 'W/"d3179e1a1f6bd91:0"',
-          Referer: "https://www.oref.org.il//12481-he/Pakar.aspx",
-          "sec-ch-ua":
-            '"Chromium";v="112", "Brave";v="112", "Not:A-Brand";v="99"',
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": '"Windows"',
-          "Sec-Fetch-Dest": "empty",
-          "Sec-Fetch-Mode": "cors",
-          "Sec-Fetch-Site": "same-origin",
-          "Sec-GPC": "1",
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-          "X-Requested-With": "XMLHttpRequest",
-        },
-      })
-        .then((result) =>
-          result.status === 304
-            ? undefined
-            : (async () => {
-                const text = await result.text();
-                try {
-                  return JSON.parse(text);
-                } catch (error) {
-                  return undefined;
-                }
-              })()
-        )
-        .then((alert) => {
-          if (alert !== undefined) {
-            let shit = fs.readFileSync("./errorsandsomeshit.txt", {
-              encoding: "utf8",
-            });
-
-            shit += alert;
-
-            fs.writeFileSync("./errorsandsomeshit.txt", shit, "utf8");
-            const json = JSON.parse(
-              fs.readFileSync("channelServer.json", {
-                encoding: "utf8",
-              })
-            );
-            for (let server in json) {
-              if (client.channels.cache.has(json[server].channel)) {
-                let channel = client.channels.cache.get(json[server].channel);
-                const embed = new EmbedBuilder()
-                  .setColor("#e8793f")
-                  .setTitle(`התרעת פיקוד העורף ב–${alert.data}`)
-                  .setURL("https://www.oref.org.il//12481-he/Pakar.aspx")
-                  .setAuthor({
-                    name: "פיקוד העורף",
-                    iconURL:
-                      "https://cdn.discordapp.com/attachments/776039568163995649/1094287528451915906/Pakar.png",
-                    url: "https://www.oref.org.il//12481-he/Pakar.aspx",
-                  })
-                  .setDescription(alert.category_desc)
-                  .addFields({
-                    name: `התרעה של פיקוד העורף בשעה–${alert.time}`,
-                    value: alert.date,
-                  });
-                channel.send({ embeds: [embed] });
+  setInterval(async () => {
+    await fetch("https://www.oref.org.il/WarningMessages/alert/alerts.json", {
+      method: "GET",
+      headers: {
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.5",
+        Connection: "keep-alive",
+        "Content-Type": "application/json;charset=utf-8",
+        "If-Modified-Since": new Date().toUTCString(),
+        "If-None-Match": 'W/"d3179e1a1f6bd91:0"',
+        Referer: "https://www.oref.org.il//12481-he/Pakar.aspx",
+        "sec-ch-ua":
+          '"Chromium";v="112", "Brave";v="112", "Not:A-Brand";v="99"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-GPC": "1",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+    })
+      .then((result) =>
+        result.status === 304
+          ? undefined
+          : (async () => {
+              const text = await result.text();
+              try {
+                return JSON.parse(text);
+              } catch (error) {
+                return undefined;
               }
-            }
-          }
-        })
-        .catch((error) => {
+            })()
+      )
+      .then((alert) => {
+        if (alert !== undefined) {
           let shit = fs.readFileSync("./errorsandsomeshit.txt", {
             encoding: "utf8",
           });
 
-          shit += error;
+          shit += alert;
 
           fs.writeFileSync("./errorsandsomeshit.txt", shit, "utf8");
+          const json = JSON.parse(
+            fs.readFileSync("channelServer.json", {
+              encoding: "utf8",
+            })
+          );
+          for (let server in json) {
+            if (client.channels.cache.has(json[server].channel)) {
+              let channel = client.channels.cache.get(json[server].channel);
+              const embed = new EmbedBuilder()
+                .setColor("#e8793f")
+                .setTitle(`התרעת פיקוד העורף ב–${alert.data}`)
+                .setURL("https://www.oref.org.il//12481-he/Pakar.aspx")
+                .setAuthor({
+                  name: "פיקוד העורף",
+                  iconURL:
+                    "https://cdn.discordapp.com/attachments/776039568163995649/1094287528451915906/Pakar.png",
+                  url: "https://www.oref.org.il//12481-he/Pakar.aspx",
+                })
+                .setDescription(alert.category_desc)
+                .addFields({
+                  name: `התרעה של פיקוד העורף בשעה–${alert.time}`,
+                  value: alert.date,
+                });
+              channel.send({ embeds: [embed] });
+            }
+          }
+        }
+      })
+      .catch((error) => {
+        let shit = fs.readFileSync("./errorsandsomeshit.txt", {
+          encoding: "utf8",
         });
-    });
-  },
-  2000
-);
+
+        shit += error;
+
+        fs.writeFileSync("./errorsandsomeshit.txt", shit, "utf8");
+
+        console.log(error);
+      });
+  }, 2000);
+});
 
 client.on(Events.InteractionCreate, async (interaction) =>
   registerCommands(interaction)
