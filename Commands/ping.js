34853b1b6d03fbbc3a3e43a492a020a8dcbbee3a
Kfir <royalplusto@gmail.com>
2023-04-15 15:53:33 +0300

    ping

---
+++ Commands/ping.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Check your ping"),
+  async execute(interaction) {
+    let startTime = new Date().getTime();
+    await interaction.reply(
+      `🏓 | Latency is: **${
+        interaction.createdTimestamp.getTime() - startTime
+      }ms.**`
+    );
+  },
+};
