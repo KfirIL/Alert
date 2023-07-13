7b92a0ea3f9328b02cd3729035f76df8edf17bbe
Kfir <royalplusto@gmail.com>
2023-07-13 21:35:38 +0300

    making crashes readable

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ client.on("ready", async () => {
     let text = fs.readFileSync("errorsandsomeshit.txt", {
       encoding: "utf8",
     });
-    text += `\n${e}`;
+    text += `\n${JSON.stringify(e)}`;
     fs.writeFileSync("errorsandsomeshit.txt", text, "utf8");
     console.log(e);
     process.exit();
@@ -87,7 +87,7 @@ client.on("ready", async () => {
     let text = fs.readFileSync("errorsandsomeshit.txt", {
       encoding: "utf8",
     });
-    text += e;
+    text += JSON.stringify(e);
     fs.writeFileSync("errorsandsomeshit.txt", text, "utf8");
     console.log(e);
     process.exit();
