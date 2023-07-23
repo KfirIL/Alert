37358a1881ed6e6fab6e72dc31a2269a07f45631
Kfir <royalplusto@gmail.com>
2023-07-23 19:48:17 +0300

    Making things readable

---
+++ alertHandler.js
@@ -0,0 +1,150 @@
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+const fs = require("node:fs");
+const WebSocket = require("ws");
+
+const WEBSOCKET_URL = "wss://ws.tzevaadom.co.il:8443/socket?platform=WEB";
+
+function writeToErrorsAndDataFile(data) {
+  let text = fs.readFileSync("errorsandsomeshit.txt", {
+    encoding: "utf8",
+  });
+  text += `\n${JSON.stringify(data)}`;
+  fs.writeFileSync("errorsandsomeshit.txt", text, "utf8");
+}
+
+async function fetchCitiesData() {
+  const versionR = await fetch("https://api.tzevaadom.co.il/lists-versions");
+  const versionJson = await versionR.json();
+  const version = versionJson.cities;
+
+  const response = await fetch(
+    "https://www.tzevaadom.co.il/static/cities.json?v=" + version
+  );
+  return response.json();
+}
+
+async function onAlert(client, m, cities, areas, countdown) {
+  const { type, data: alert } = JSON.parse(m.data);
+  if (type !== "ALERT") return;
+  if (data.threat !== 0 && data.threat !== 2) return;
+  writeToErrorsAndDataFile(alert);
+
+  // Reloading json file.
+  const json = JSON.parse(
+    fs.readFileSync("channelServer.json", {
+      encoding: "utf8",
+    })
+  );
+
+  const title =
+    threat === 0
+      ? "ירי טילים ורקטות"
+      : threat === 2
+      ? "חשש לחדירת מחבל"
+      : undefined;
+
+  const embed = new EmbedBuilder()
+    .setColor("#ff3d00")
+    .setTitle(title)
+    .setDescription("היכנסו למרחב המוגן ושהו בו 10 דקות")
+    .setURL("https://www.oref.org.il//12481-he/Pakar.aspx")
+    .setAuthor({
+      name: "מנהל ההתרעות של ישראל",
+    })
+    .setThumbnail(
+      "https://cdn.discordapp.com/attachments/1041017624299048981/1132264780481187860/Alert-Logo.png"
+    )
+    .addFields(
+      {
+        name: "אזור בארץ:",
+        value: areas[cities[alert.cities[0]].area]["he"],
+      },
+      { name: "\u200B", value: "\u200B" },
+      {
+        name: "יישובים:",
+        value: "​", // Whitespace character
+      }
+    )
+    .setFooter({
+      text: "התוכן לא מהווה תחליף להתרעות בזמן אמת. כדי לקבל התרעות מדוייקות נא להיכנס לאתר פיקוד העורף.",
+    })
+    .setTimestamp(new Date(alert.time * 1000));
+
+  for (let city in alert.cities) {
+    const cityCountDown = cities[alert.cities[city]].countdown;
+
+    embed.addFields({
+      name: alert.cities[city],
+      value: `זמן כניסה למ"מ: ${countdown[cityCountDown]["he"]}`,
+      inline: true,
+    });
+  }
+  embed.addFields({ name: "\u200B", value: "\u200B" });
+
+  for (let server in json) {
+    const channel = client.guild.channels.cache.has(json[server].channel)
+      ? client.guild.channels.cache.get(json[server].channel)
+      : undefined;
+
+    if (client == undefined) return;
+    else if (
+      !channel
+        .permissionsFor(interaction.guild.members.me)
+        .has(PermissionsBitField.Flags.SendMessages)
+    )
+      return;
+
+    channel.send({
+      embeds: [embed],
+      content: client.guilds.cache
+        .get(server)
+        .roles.cache.has(json[server].role)
+        ? client.guilds.cache.get(server).roles.cache.get(json[server].role)
+            .name !== "@everyone"
+          ? `<@&${json[server].role}>`
+          : "@everyone"
+        : undefined,
+    });
+  }
+}
+
+async function wsConnect(client) {
+  const citiesData = await fetchCitiesData();
+
+  let ws = new WebSocket(WEBSOCKET_URL, {
+    headers: {
+      origin: "https://www.tzevaadom.co.il",
+    },
+  });
+
+  function reconnect(ws) {
+    ws = new WebSocket(WEBSOCKET_URL, {
+      headers: {
+        origin: "https://www.tzevaadom.co.il",
+      },
+    });
+  }
+  ws.onopen = () => {
+    console.log("ws connected");
+  };
+  ws.onclose = () => {
+    console.log("\n\n\nClosed");
+    reconnect(ws);
+  };
+
+  ws.onerror = (e) => {
+    writeToErrorsAndDataFile(e);
+    console.log(`Error: ${JSON.stringify(e)}`);
+  };
+
+  ws.onmessage = (m) =>
+    onAlert(
+      client,
+      m,
+      citiesData.cities,
+      citiesData.areas,
+      citiesData.countdown
+    );
+}
+
+module.exports = wsConnect;
--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 require("dotenv").config();
-
 const fs = require("node:fs");
+const { Client, GatewayIntentBits, Events } = require("discord.js");
 const { registerCommands, registerButtons } = require("./reg.js");
-const WebSocket = require("ws");
-const WEBSOCKET_URL = "wss://ws.tzevaadom.co.il:8443/socket?platform=WEB";
-
-const {
-  Client,
-  GatewayIntentBits,
-  EmbedBuilder,
-  Events,
-  PermissionsBitField,
-} = require("discord.js");
+const { wsConnect } = require("./alertHandler.js");
 
 const client = new Client({
   intents: [
@@ -30,32 +21,13 @@ client.on("ready", async () => {
     type: parseInt(process.env.STATUS_CATEGORY),
   });
 
-  const versionR = await fetch("https://api.tzevaadom.co.il/lists-versions");
-  const versionJson = await versionR.json();
-  const version = versionJson.cities;
-
-  fetch("https://www.tzevaadom.co.il/static/cities.json?v=" + version)
-    .then((response) => response.json())
-    .then((data) => {
-      fs.writeFileSync("cities.json", JSON.stringify(data), "utf8");
-    });
-
-  let json = JSON.parse(
+  const json = JSON.parse(
     fs.readFileSync("channelServer.json", {
       encoding: "utf8",
     })
   );
 
-  const jsonCities = JSON.parse(
-    fs.readFileSync("cities.json", {
-      encoding: "utf8",
-    })
-  );
-
-  const cities = jsonCities.cities;
-  const areas = jsonCities.areas;
-  const countdown = jsonCities.countdown;
-
+  // Checking for any servers that were left
   for (let server in json) {
     if (!client.guilds.cache.has(server)) {
       delete json[server];
@@ -65,122 +37,10 @@ client.on("ready", async () => {
     }
   }
 
-  let ws;
-
-  function connect() {
-    ws = new WebSocket(WEBSOCKET_URL, {
-      headers: {
-        origin: "https://www.tzevaadom.co.il",
-      },
-    });
-  }
-
-  connect();
-
-  ws.onopen = () => {
-    console.log("ws connected");
-  };
-
-  ws.onclose = () => {
-    console.log("\n\n\nClosed");
-    connect;
-  };
-
-  ws.onerror = (e) => {
-    let text = fs.readFileSync("errorsandsomeshit.txt", {
-      encoding: "utf8",
-    });
-    text += JSON.stringify(e);
-    fs.writeFileSync("errorsandsomeshit.txt", text, "utf8");
-    console.log(`Error: ${JSON.stringify(e)}`);
-  };
-
-  ws.onmessage = async (m) => {
-    if (typeof m.data != "string") return;
-    const { type, data } = JSON.parse(m.data);
-    console.log(type);
-    console.log(data);
-    if (type !== "ALERT") return;
-    if (data.threat !== 0) return;
-    const alert = data;
-    let text = fs.readFileSync("errorsandsomeshit.txt", {
-      encoding: "utf8",
-    });
-    text += `\n${JSON.stringify(alert)}`;
-    fs.writeFileSync("errorsandsomeshit.txt", text, "utf8");
-
-    json = JSON.parse(
-      fs.readFileSync("channelServer.json", {
-        encoding: "utf8",
-      })
-    );
-
-    const embed = new EmbedBuilder()
-      .setColor("#ff3d00")
-      .setTitle("ירי טילים ורקטות")
-      .setDescription("היכנסו למרחב המוגן ושהו בו 10 דקות")
-      .setURL("https://www.oref.org.il//12481-he/Pakar.aspx")
-      .setAuthor({
-        name: "מנהל ההתרעות של ישראל",
-      })
-      .setThumbnail(
-        "https://cdn.discordapp.com/attachments/1041017624299048981/1132264780481187860/Alert-Logo.png"
-      )
-      .addFields(
-        {
-          name: "אזור בארץ:",
-          value: areas[cities[alert.cities[0]].area]["he"],
-        },
-        { name: "\u200B", value: "\u200B" },
-        {
-          name: "יישובים:",
-          value: "​", // Whitespace character
-        }
-      )
-      .setFooter({
-        text: "התוכן לא מהווה תחליף להתרעות בזמן אמת. כדי לקבל התרעות מדוייקות נא להיכנס לאתר פיקוד העורף.",
-      })
-      .setTimestamp(new Date(alert.time * 1000));
-
-    for (let city in alert.cities) {
-      const cityCountDown = cities[alert.cities[city]].countdown;
-
-      embed.addFields({
-        name: alert.cities[city],
-        value: `זמן כניסה למ"מ: ${countdown[cityCountDown]["he"]}`,
-        inline: true,
-      });
-    }
-    embed.addFields({ name: "\u200B", value: "\u200B" });
-
-    for (let server in json) {
-      const channel = client.guild.channels.cache.has(json[server].channel)
-        ? client.guild.channels.cache.get(json[server].channel)
-        : undefined;
-
-      if (client == undefined) return;
-      else if (
-        !channel
-          .permissionsFor(interaction.guild.members.me)
-          .has(PermissionsBitField.Flags.SendMessages)
-      )
-        return;
-
-      channel.send({
-        embeds: [embed],
-        content: client.guilds.cache
-          .get(server)
-          .roles.cache.has(json[server].role)
-          ? client.guilds.cache.get(server).roles.cache.get(json[server].role)
-              .name !== "@everyone"
-            ? `<@&${json[server].role}>`
-            : "@everyone"
-          : undefined,
-      });
-    }
-  };
+  wsConnect(client);
 });
 
+// Interactions
 client.on(Events.InteractionCreate, async (interaction) => {
   registerButtons(interaction);
   registerCommands(interaction);
